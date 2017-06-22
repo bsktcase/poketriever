bf49f22da4f999eb8ea68d56a0f14cc23660d16d
bsktcase <bsktcase@bsktcase.com>
2017-06-21 23:38:02 -0700

    refactor formulae in hopes of making them easier to maintain; add and remove a thing to delete empty rows in sheet (undes side effects)

--- index-up.js
+++ index-up.js
@@ -7,6 +7,7 @@ welcome.welcome();
 
 auth.authenticate()
     .then(gsheets.clearSheets)
+    // .then(gsheets.deleteRows)
     .then(gsheets.doTheThing)
     .then(gsheets.updateFormulae)
     .catch(console.err);
